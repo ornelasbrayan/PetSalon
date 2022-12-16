92fa4b6c1d8e8509711100a7a2d554a793b5f237
ornelasbrayan <ornelas.brayan@uabc.edu.mx>
2022-12-15 21:00:17 -0700

    Services

---
+++ scripts/services.js
@@ -0,0 +1,19 @@
+let services={
+    nailtrim:{
+        price:"$15"
+    },
+    nailgrinding:{
+        price:"$20"
+    },
+    massage:{
+        price:"$20"
+    },
+    pawdicure:{
+        price:"$15"
+    },
+    dentalcleaning:{
+        price:"$15"
+    },
+
+
+}
