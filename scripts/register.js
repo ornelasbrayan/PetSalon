cb1476eedfedd9ee8f7db8b0224a55456bbcfc27
ornelasbrayan <ornelas.brayan@uabc.edu.mx>
2022-12-15 20:28:08 -0700

    Pet Salon
    Register done

---
+++ scripts/register.js
@@ -0,0 +1,14 @@
+let salon={
+    name:"The Fashion Pet",
+    adress:{
+        street:"Palomar",
+        zip:92015,
+        number:500
+    },
+    hours:{
+        open:"9:00 am",
+        close:"5:00 pm"
+    }
+}
+
+console.log(salon);
