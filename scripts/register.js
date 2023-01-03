88067f368853deb6a55472a992820c74da7e8288
ornelasbrayan <ornelas.brayan@uabc.edu.mx>
2023-01-03 15:33:21 -0800

    petsalon-objectliteral

--- scripts/register.js
+++ scripts/register.js
@@ -1,5 +1,8 @@
-let salon={
+//object literal
+// name, age, gender, breed, service, owner, contact phone
+let petSalon={
     name:"The Fashion Pet",
+    phone: "555-555-555",
     adress:{
         street:"Palomar",
         zip:92015,
@@ -8,7 +11,37 @@ let salon={
     hours:{
         open:"9:00 am",
         close:"5:00 pm"
-    }
+    },
+    pets:[
+        {
+            name:"Scooby",
+            age: 50,
+            gender:"Male",
+            breed:"Dane",
+            servide:"Grooming",
+            owner:"Shaggy",
+            contactPhone:"555-555-555"
+        },
+        {
+            name:"Bobby",
+            age: 2,
+            gender:"Male",
+            breed:"Chihuahua",
+            servide:"Shower",
+            owner:"Brayan",
+            contactPhone:"664-553-534"
+        }
+    ]
 }
 
-console.log(salon);
+function displayInfo(){
+
+}
+
+function init(){
+    // main
+    console.log(patSalon.name);
+
+}
+
+window.onload = init;
